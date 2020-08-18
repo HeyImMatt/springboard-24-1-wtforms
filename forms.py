9a906cdab26f64b73dcb7cb45f7c95b295c0baa7
heyimmatt <mrmcfarland@gmail.com>
2020-08-18 16:54:57 -0700

    Turn species into select field and add URL and NumberRange validators

--- forms.py
+++ forms.py
@@ -1,12 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, TextAreaField
-from wtforms.validators import InputRequired
+from wtforms import StringField, IntegerField, TextAreaField, SelectField
+from wtforms.validators import InputRequired, URL, NumberRange
 
 class AddPetForm(FlaskForm):
     """Form for adding pets"""
 
-    name = StringField("Pet Name", validators=[InputRequired()])
-    species = StringField("Species", validators=[InputRequired()])
-    photo_url = StringField("Photo URL")
-    age = IntegerField("Age")
+    name = StringField("Pet Name", validators=[InputRequired(message="Pet must have a name")])
+    species = SelectField("Species", choices=[('cat', 'Cat'), ('dog', 'Dog'), ('porcupine', 'Porcupine')], validators=[InputRequired()])
+    photo_url = StringField("Photo URL", validators=[URL(message="Must be a valid URL")])
+    age = IntegerField("Age", validators=[NumberRange(min=0, max=30, message="Must be between 0 and 30")])
     notes = TextAreaField("Notes")
