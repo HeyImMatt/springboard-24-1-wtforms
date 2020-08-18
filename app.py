13e90f271335250e065f8c13b2f7b2b09521ec73
heyimmatt <mrmcfarland@gmail.com>
2020-08-18 12:02:42 -0700

    Initialize repo

---
+++ app.py
@@ -0,0 +1,16 @@
+"""Adopt Project Exercise"""
+
+from flask import Flask, request, redirect, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///adopt'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = "adopt1234"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
+
+connect_db(app)
